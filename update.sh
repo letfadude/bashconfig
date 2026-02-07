d69d588020b0db2775297dd187d0404a8be78a13
letfadude <wallneralex7789@gmail.com>
2026-02-07 13:39:25 +0100

    update update

--- update.sh
+++ update.sh
@@ -9,15 +9,12 @@ user_home=$(pwd)
 cd -
 
 # starship
-rm -rf $user_home/.config/starship.toml
-ln -s $config_path/starship.toml $user_home/.config/starship.toml
+ln -sf $config_path/starship.toml $user_home/.config/starship.toml
 
-rm -rf ~/.config/nvim
-ln -s $config_path/nvim $user_home/.config/nvim
+ln -sf $config_path/nvim $user_home/.config/nvim
 
 # Use new bashrc
-rm -f $user_home/.bashrc 
-ln -s $config_path/.bashrc $user_home/.bashrc
+ln -sf $config_path/.bashrc $user_home/.bashrc
 
 source ~/.bashrc
 
