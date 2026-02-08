2948bf80e92775b5a6f1cf8f0434e03e8e1ee61c
alex <wallneralex7789@gmail.com>
2026-02-08 13:41:41 +0100

    update script nice

--- install.sh
+++ install.sh
@@ -1,88 +1,162 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
+logfile="install.log"
 
-# Install git
-sudo apt install git
+echo "Starting install script" > "$logfile"
 
-# Install curl
-sudo apt install curl
+packages=(git curl wget tilix bat tmux xsel wl-clipboard xclip clangd ripgrep latexmk zathura texlive-full)
+echo "install apt packages $packages ? {y/n}"
+read inst
+i=0
+if [ $inst == 'y' ]
+then
+  echo "*** UPDATING PACKAGE MANAGER ***" | tee -a "$logfile"
+  sudo apt update >> "$logfile" 
+  sudo apt upgrade >> "$logfile"
+  echo "*** INSTALLING APT PACKAGES ***" | tee -a "$logfile"
 
-# Install tilix
-sudo apt install tilix
+  for pkg in "${packages[@]}"; 
+  do 
+    ((i++))
+    echo "** installing $pkg **" | tee -a "$logfile"
+    sudo apt install $pkg -y >> "$logfile"
+    if [ $? -ne 0 ]
+    then
+      echo "*** INSTALL FAILED [$pkg]***" | tee -a "$logfile"
+    else 
+      echo "** $pkg installed successfully **" | tee -a "$logfile"
+      echo "-" | tee -a "$logfile"
+    fi
+  done
+fi
 
-git clone https://codeberg.org/SnowCode/tilix-colors.git
-mkdir -p ~/.config/tilix/schemes
-mv tilix-colors/*.json ~/.config/tilix/schemes
-rm -r tilix-colors
+echo "*** INSTALLING $i APT PACKAGES DONE ***" | tee -a "$logfile"
+echo "proceed to addon installations ? [y/n]"
+read proceed
+if [ $proceed != 'y' ]
+then 
+  echo "Addon installations skipped" | tee -a "$logfile"
+  exit 0
+fi
+  
+echo "*** ADDONS INSTALLATION ***" | tee -a "$logfile"
+addon="tilix"
+echo "install $addon? [y/n]"
+read inst
+if [ $inst == 'y' ]
+then
+  echo "*** INSTALLING TILIX COLOR SCHEMES ***" | tee -a "$logfile"
+  git clone https://codeberg.org/SnowCode/tilix-colors.git
+  mkdir -p ~/.config/tilix/schemes
+  mv tilix-colors/*.json ~/.config/tilix/schemes
+  rm -r tilix-colors
+else 
+  echo "$addon installation skipped" | tee -a "$logfile"
+fi
 
-# Install fonts
-mkdir -p ~/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip -O ~/Downloads/JetBrainsMono.zip
-unzip ~/Downloads/JetBrainsMono.zip -d ~/.local/share/fonts
+addon="jetbrains-font"
+echo "install $addon? [y/n]"
+read inst
+if [ $inst == 'y' ]
+then
+  echo "*** INSTALLING JETBRAINS FONT ***" | tee -a "$logfile"
+  # Install fonts
+  mkdir -p ~/.local/share/fonts
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip -O ~/Downloads/JetBrainsMono.zip
+  unzip ~/Downloads/JetBrainsMono.zip -d ~/.local/share/fonts
+else 
+  echo "$addon installation skipped" | tee -a "$logfile"
+fi
 
-# Install starship
-curl -sS https://starship.rs/install.sh | sh
+addon="starship"
+echo "install $addon? [y/n]"
+read inst
+if [ $inst == 'y' ]
+then
+  echo "*** INSTALLING STARSHIP ***" | tee -a "$logfile"
+  curl -sS https://starship.rs/install.sh | sh
+else 
+  echo "$addon installation skipped" | tee -a "$logfile"
+fi
 
-# Install bat
-curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
-bash ble-nightly/ble.sh --install ~/.local/share
-rm -rf ble-nightly
+addon="ble-nighlty"
+echo "install $addon? [y/n]"
+read inst
+if [ $inst == 'y' ]
+then
+  echo "*** INSTALLING BLE-NIGHTLY ***" | tee -a "$logfile"
+  curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
+  bash ble-nightly/ble.sh --install ~/.local/share
+  rm -rf ble-nightly
+else 
+  echo "$addon installation skipped" | tee -a "$logfile"
+fi
 
-# Install bat
-sudo apt install bat -y
+# Install tmux with catpuccin
 
-# Install tmux
-sudo apt install tmux -y
-
-mkdir -p ~/.config/tmux/plugins/catppuccin
-git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+addon="tmux catppuccin theme"
+echo "install $addon? [y/n]"
+read inst
+if [ $inst == 'y' ]
+then
+  echo "*** SETTING TMUX COLOR SCHEME TO CATPPUCCIN ***" | tee -a "$logfile"
+  mkdir -p ~/.config/tmux/plugins/catppuccin
+  git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+  # color
+  mkdir -p ~/.config/tmux/plugins/catppuccin
+else
+  echo "$addon installation skipped" | tee -a "$logfile"
+fi
 
 # Install neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim-linux-x86_64
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-
-# Clipboard
-sudo apt install xsel -y
-
-# color
-mkdir -p ~/.config/tmux/plugins/catppuccin
+addon="nvim"
+echo "install $addon? [y/n]"
+read inst
+if [ $inst == 'y' ]
+then
+  echo "*** INSTALLING NVIM ***" | tee -a "$logfile"
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+  sudo rm -rf /opt/nvim-linux-x86_64
+  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+  sudo rm nvim-linux-x86_64.tar.gz
+else
+  echo "$addon installation skipped" | tee -a "$logfile"
+fi
 
 # install go 
-wget https://go.dev/dl/go1.25.5.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.25.5.linux-amd64.tar.gz
-rm gol.25.5.linux-amd64.tar.gz
+addon="golang and golang-langserver"
 
-# golang support for vim
-go install github.com/nametake/golangci-lint-langserver@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+echo "install $addon? [y/n]"
+read inst
+if [ $inst == 'y' ]
+then
+  echo "*** INSTALLING GOLANG ***" | tee -a "$logfile"
+  wget https://go.dev/dl/go1.25.5.linux-amd64.tar.gz
+  sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.25.5.linux-amd64.tar.gz
+  rm go1.25.5.linux-amd64.tar.gz
 
-# C support for vim
-sudo apt install clangd -y
+  # golang support for vim
+  echo "*** INSTALLING GOLANG SUPPORT FOR NVIM ***" | tee -a "$logfile"
+  go install github.com/nametake/golangci-lint-langserver@latest
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+else
+  echo "$addon installation skipped" | tee -a "$logfile"
+fi
 
-# arduino support for vim
-cd ~
-mkdir -p ~/arduino-cli
-cd ~/arduino-cli
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh 
-sudo mv ./bin/arduino-cli /usr/local/bin/arduino-cli
+addon="arduino-cli"
+echo "install $addon? [y/n]"
+read inst
+if [ $inst == 'y' ]
+then
+  # arduino support for vim
+  echo "*** INSTALLING ARDUINO_CLI ***" | tee -a "$logfile"
+  cd ~
+  mkdir -p ~/arduino-cli
+  cd ~/arduino-cli
+  curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh 
+  sudo mv ./bin/arduino-cli /usr/local/bin/arduino-cli
+else
+  echo "$addon installation skipped" | tee -a "$logfile"
+fi
 
-## install ripgrep for telescope
-sudo apt install ripgrep -y
-## install latexmk for vimtex
-sudo apt install latexmk -y
-## install zathura for pdf viewing
-sudo apt install zathura -y
-## TEX stuff
-sudo apt-get install texlive-latex-recommended texlive-latex-extra -y
-sudo apt-get install texlive-koma-script -y
-sudo apt install biber -y
-sudo apt install texlive-bibtex-extra -y
-sudo apt install texlive-lang-english -y
-
-## texlab lsp setup : download from : https://github.com/latex-lsp/texlab/releases
-# tar -xvzf texlab-x86_64-linux.tar.gz
-# sudo mv texlab /usr/local/bin/texlab
-
+echo "*** INSTALLATION COMPLETE ***"
--- update.sh
+++ update.sh
@@ -10,14 +10,40 @@ cd -
 
 # starship
 ln -sf $config_path/starship.toml $user_home/.config/starship.toml
+if [ $? -ne 0 ]
+then
+  echo "ERR: startship config not linked"
+else 
+  echo "starship config linked"
+fi
 
 ln -sf $config_path/nvim $user_home/.config/nvim
+
+if [ $? -ne 0 ]
+then
+  echo "ERR: nvim config not linked"
+else 
+  echo "nvim config linked"
+fi
 
 # Use new bashrc
 ln -sf $config_path/.bashrc $user_home/.bashrc
 
+if [ $? -ne 0 ]
+then
+  echo "ERR: bashrc config not linked"
+else 
+  echo "bashrc config linked"
+fi
+
 dconf load / < dconf.txt
+if [ $? -ne 0 ]
+then
+  echo "ERR: dconf not loaded"
+else 
+  echo "dconf loaded"
+fi
 
 source ~/.bashrc
 
-echo done
+echo 'done'
