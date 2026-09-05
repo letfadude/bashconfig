5f4c718da53c39495835d8df58dbd649a36b883f
fadude <wallneralex7789@gmail.com>
2026-09-05 11:44:07 +0200

    brave, wg, buildtools, ..

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 logfile="install.log"
-packages=(git curl wget tilix bat tmux xsel wl-clipboard xclip clangd ripgrep latexmk zathura texlive-full texlab flatpak openvpn podman erlang rebar3 elixir)
+packages=(git curl wget tilix bat tmux xsel wl-clipboard xclip clangd ripgrep latexmk zathura texlive-full texlab flatpak openvpn podman erlang rebar3 elixir build-essential mosquitto mosquitto-clients wireguard wireguard-tools)
 
 echo "Starting install script .." > "$logfile"
 echo "apt packages: "
@@ -36,12 +36,14 @@ then
   done
 fi
 
+./install_brave.sh 2>&1 >> "$logfile"
+
 echo "*** INSTALLING $i APT PACKAGES DONE ***" | tee -a "$logfile"
 echo "add default flatpak repository ? [y/n]"
 read inst
 if [ "$inst" == 'y' ]
 then
-  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 2>&1 >> "$logfile"
 fi
 
 echo "proceed to addon installations ? [y/n]"
@@ -54,16 +56,17 @@ fi
 %lets see
   
 echo "*** ADDONS INSTALLATION ***" | tee -a "$logfile"
-addon="tilix"
+
+addon="tilix colors"
 echo "install $addon? [y/n]"
 read inst
 if [ "$inst" == 'y' ]
 then
   echo "*** INSTALLING [$addon] ***" | tee -a "$logfile"
-  git clone https://codeberg.org/SnowCode/tilix-colors.git
-  mkdir -p ~/.config/tilix/schemes
-  mv tilix-colors/*.json ~/.config/tilix/schemes
-  rm -r tilix-colors
+  git clone https://codeberg.org/SnowCode/tilix-colors.git 2>&1 >> "$logfile"
+  mkdir -p ~/.config/tilix/schemes 2>&1 >> "$logfile"
+  mv tilix-colors/*.json ~/.config/tilix/schemes 2>&1 >> "$logfile"
+  rm -r tilix-colors 2>&1 >> "$logfile"
 else 
   echo "$addon installation skipped" | tee -a "$logfile"
 fi
@@ -76,8 +79,8 @@ then
   echo "*** INSTALLING [$addon] ***" | tee -a "$logfile"
   # Install fonts
   mkdir -p ~/.local/share/fonts
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip -O ~/Downloads/JetBrainsMono.zip
-  unzip ~/Downloads/JetBrainsMono.zip -d ~/.local/share/fonts
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip -O ~/Downloads/JetBrainsMono.zip 2>&1 >> "$logfile"
+  unzip ~/Downloads/JetBrainsMono.zip -d ~/.local/share/fonts 2>&1 >> "$logfile"
 else 
   echo "$addon installation skipped" | tee -a "$logfile"
 fi
@@ -88,7 +91,7 @@ read inst
 if [ "$inst" == 'y' ]
 then
   echo "*** INSTALLING [$addon] ***" | tee -a "$logfile"
-  curl -sS https://starship.rs/install.sh | sh
+  curl -sS https://starship.rs/install.sh | sh 2>&1 >> "$logfile"
   if [ $? -ne 0 ]
   then 
     echo "installing addon [$addon] failed"
@@ -103,25 +106,25 @@ read inst
 if [ "$inst" == 'y' ]
 then
   echo "*** INSTALLING [$addon] ***" | tee -a "$logfile"
-  curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
-  bash ble-nightly/ble.sh --install ~/.local/share
-  rm -rf ble-nightly
+  curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - 2>&1 >> "$logfile"
+  bash ble-nightly/ble.sh --install ~/.local/share 2>&1 >> "$logfile"
+  rm -rf ble-nightly 2>&1 >> "$logfile"
 else 
   echo "$addon installation skipped" | tee -a "$logfile"
 fi
 
-# Install tmux with catppuccin
 
+# Install tmux with catppuccin
 addon="tmux catppuccin theme"
 echo "install $addon? [y/n]"
 read inst
 if [ "$inst" == 'y' ]
 then
   echo "*** SETTING TMUX COLOR SCHEME TO CATPPUCCIN ***" | tee -a "$logfile"
-  mkdir -p ~/.config/tmux/plugins/catppuccin
-  git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+  mkdir -p ~/.config/tmux/plugins/catppuccin 2>&1 >> "$logfile"
+  git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux 2>&1 >> "$logfile"
   # color
-  mkdir -p ~/.config/tmux/plugins/catppuccin
+  mkdir -p ~/.config/tmux/plugins/catppuccin 2>&1 >> "$logfile"
 else
   echo "$addon installation skipped" | tee -a "$logfile"
 fi
@@ -133,10 +136,10 @@ read inst
 if [ "$inst" == 'y' ]
 then
   echo "*** INSTALLING [$addon] ***" | tee -a "$logfile"
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-  sudo rm -rf /opt/nvim-linux-x86_64
-  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-  sudo rm nvim-linux-x86_64.tar.gz
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz 2>&1 >> "$logfile"
+  sudo rm -rf /opt/nvim-linux-x86_64 2>&1 >> "$logfile"
+  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz 2>&1 >> "$logfile"
+  sudo rm nvim-linux-x86_64.tar.gz 2>&1 >> "$logfile"
 else
   echo "$addon installation skipped" | tee -a "$logfile"
 fi
@@ -149,15 +152,15 @@ read inst
 if [ "$inst" == 'y' ]
 then
   echo "*** INSTALLING [$addon] ***" | tee -a "$logfile"
-  wget https://go.dev/dl/go1.25.5.linux-amd64.tar.gz
-  sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.25.5.linux-amd64.tar.gz
-  rm go1.25.5.linux-amd64.tar.gz
+  wget https://go.dev/dl/go1.25.5.linux-amd64.tar.gz 2>&1 >> "$logfile"
+  sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.25.5.linux-amd64.tar.gz 2>&1 >> "$logfile"
+  rm go1.25.5.linux-amd64.tar.gz 2>&1 >> "$logfile"
 
   # golang support for vim
   echo "*** INSTALLING GOLANG SUPPORT FOR NVIM ***" | tee -a "$logfile"
-  go install golang.org/x/tools/gopls@latest
-  go install github.com/nametake/golangci-lint-langserver@latest
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+  go install golang.org/x/tools/gopls@latest 2>&1 >> "$logfile"
+  go install github.com/nametake/golangci-lint-langserver@latest 2>&1 >> "$logfile"
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest 2>&1 >> "$logfile"
 else
   echo "$addon installation skipped" | tee -a "$logfile"
 fi
@@ -172,8 +175,8 @@ then
   cd ~
   mkdir -p ~/arduino-cli
   cd ~/arduino-cli
-  curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh 
-  sudo mv ./bin/arduino-cli /usr/local/bin/arduino-cli
+  curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh 2>&1 >> "$logfile"
+  sudo mv ./bin/arduino-cli /usr/local/bin/arduino-cli 2>&1 >> "$logfile"
 else
   echo "$addon installation skipped" | tee -a "$logfile"
 fi
@@ -190,7 +193,7 @@ then
 fi
 
 echo "*** UPDATING CONFIG ***"
-/bin/bash update.sh
+/bin/bash ./update.sh
 if [ $? -ne 0 ]
 then
   echo "update failed try again <update.sh>"
